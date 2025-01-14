2d98ff7fbbbfc77bb5dfc67cb118873abfd91f57
MichalHir <michalaizi@gmail.com>
2025-01-14 17:19:07 +0200

    requirements.txt

--- back_end/settings.py
+++ back_end/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-reklcc60mf76$1prpc$h4cw8l_%q8*45!6-5x=l1!g+p67@9n@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
